5f73b63620c39765e81b0f4959eb4fa1382bf623
Erick Guzman <guzmi.dev@gmail.com>
2022-04-16 00:28:43 -0600

    :art: mapActions

--- src/components/CounterPage.vue
+++ src/components/CounterPage.vue
@@ -5,7 +5,7 @@
 
   <button @click="increment">+1</button>
   <button @click="incrementBy">+5</button>
-  <button @click="incrementRandomInt">Random</button>
+  <button @click="randomInt">Random</button>
 
   <h1>mapState</h1>
   <h2>mapState: {{ count }}</h2>
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapState, mapActions } from "vuex";
 
 export default {
   computed: {
@@ -23,7 +23,7 @@ export default {
     ...mapState(["count", "lastMutation"]),
     // ...mapState({
     //   count: (state) => state.count,
-    //   lastMutation: (state) => state.lastMutation,
+    //   lastMutation: 'lastMutation',
     // }),
   },
   methods: {
@@ -32,10 +32,15 @@ export default {
     },
     incrementBy() {
       this.$store.commit("incrementBy", 5);
+      // this.randomInt()
     },
-    incrementRandomInt() {
-      this.$store.dispatch("incrementRandomInt");
-    },
+    // incrementRandomInt() {
+    //   this.$store.dispatch("incrementRandomInt");
+    // },
+    // ...mapActions(["incrementRandomInt"]),
+    ...mapActions({
+      randomInt: "incrementRandomInt",
+    }),
   },
 };
 </script>
